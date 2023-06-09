ddf719ca8a6ea8d3e9cca4bb056eee8f3bdcb6f6
Yergeni Cabranes <yergenis@gmail.com>
2023-06-09 16:50:00 -0400

    fix: explicit added the backend deployed URL

--- frontend/src/common/constants.ts
+++ frontend/src/common/constants.ts
@@ -1,6 +1,7 @@
 // import.meta.env.VITE_SERVER_URL will get the values from the Heroku backend deployment
 // https://auth-mern-boilerplate.herokuapp.com
-export const API_BASE_URL = import.meta.env.VITE_SERVER_URL || "http://localhost:5000";
+export const API_BASE_URL = "https://auth-mern-boilerplate.herokuapp.com";
+// export const API_BASE_URL = import.meta.env.VITE_SERVER_URL || "http://localhost:5000";
 
 export const ROUTES = {
 	HOME: "/",
