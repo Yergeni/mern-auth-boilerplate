f64f9829c75e6a66ad4bf72ae08e02e2e5e882e8
Yergeni Cabranes <yergenis@gmail.com>
2023-06-13 14:23:44 -0400

    fix: deployment server URL

--- frontend/src/context/slices/apiSlice.ts
+++ frontend/src/context/slices/apiSlice.ts
@@ -1,10 +1,12 @@
 /**
  * More info: https://redux-toolkit.js.org/rtk-query/overview
  */
-
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
+import { API_BASE_URL } from "../../common/constants";
 
-const baseQuery = fetchBaseQuery({ baseUrl: "" }); // Empty string since there is a proxy in line #9 from `vite.config.ts`
+// Empty string since there is a proxy in line #9 from `vite.config.ts`
+// const baseQuery = fetchBaseQuery({ baseUrl: "" }); 
+const baseQuery = fetchBaseQuery({ baseUrl: API_BASE_URL });
 
 export const apiSlice = createApi({
 	baseQuery,
